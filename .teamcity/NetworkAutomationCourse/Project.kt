fb02de67baf4cd811054484075d13b85d1ce8ac2
SamerLabban <SamerLabban@TeamCity>
2017-08-23 15:50:52 -0400

    TeamCity change in 'Network Automation Course' project: Using version control synchronization settings from a parent project

--- .teamcity/NetworkAutomationCourse/Project.kt
+++ .teamcity/NetworkAutomationCourse/Project.kt
@@ -3,9 +3,6 @@ package NetworkAutomationCourse
 import NetworkAutomationCourse.buildTypes.*
 import jetbrains.buildServer.configs.kotlin.v10.*
 import jetbrains.buildServer.configs.kotlin.v10.Project
-import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.VersionedSettings
-import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.VersionedSettings.*
-import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.versionedSettings
 
 object Project : Project({
     uuid = "5292ab8b-61b0-4dc5-8e86-e25cdee869f7"
@@ -14,16 +11,4 @@ object Project : Project({
     name = "Network Automation Course"
 
     buildType(NetworkAutomationCourse_Build)
-
-    features {
-        versionedSettings {
-            id = "PROJECT_EXT_6"
-            mode = VersionedSettings.Mode.ENABLED
-            buildSettingsMode = VersionedSettings.BuildSettingsMode.USE_CURRENT_SETTINGS
-            rootExtId = "NetworkAutomationCourse_HttpsGithubComSamerLabbanNetworkAutomationCourseRefsHead"
-            showChanges = false
-            settingsFormat = VersionedSettings.Format.KOTLIN
-            param("credentialsStorageType", "credentialsJSON")
-        }
-    }
 })
